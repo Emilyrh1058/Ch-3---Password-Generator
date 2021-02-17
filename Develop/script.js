e44e71765cb0c9f8f845f609dcc06260a72db9e0
Emily Salinas <emilyrh1058@gmail.com>
2021-02-17 01:19:27 -0600

    added variables and psuedo

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,23 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+var numbers = "0123456789"
+var specialCharacters = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+
+
+// Assignment code here
+
+
+
+// Password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
-}
+};
 
-// Add event listener to generate button
+// Event listener to generate button
 generateBtn.addEventListener("click", writePassword);
- 
-
-
-test
