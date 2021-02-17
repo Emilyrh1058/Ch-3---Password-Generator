927769ea96d2bdd199e4ac0face682858ba0d441
Emily Salinas <emilyrh1058@gmail.com>
2021-02-17 02:15:33 -0600

    added concats

--- Develop/script.js
+++ Develop/script.js
@@ -6,36 +6,56 @@ var specialCharacters = ["!#$%&'()*+,-./:;<=>?@[]^_`{|}~"]
 
 // PASSWORD GENERATOR QUESTIONS
 
-// LENGTH OPTION
-function generatePassword(){
-  length = parseInt(window.prompt("How many characters would you like your password to be? Please choose between 8 and 128."));
-  if (length < 7 || length > 128) {
-    alert("Not a valid response, refresh the page and try again!")
-    }
+  // LENGTH OPTION
+  function generatePassword () {
+    length = parseInt (window.prompt ("How many characters would you like your password to be? Please choose between 8 and 128."));
+    if (length < 7 || length > 128) {
+      alert("Not a valid response, refresh the page and try again!")
+      }
     else {
 
-// LETTER CASE OPTION
-  uppercase = confirm ("Do you want to include uppercase letters? Please select OK for 'Yes' or Cancel for 'No'")
-    console.log (uppercase);
+  // LETTER CASE OPTION
+    upperCase = confirm ("Do you want to include uppercase letters? Please select OK for 'Yes' or Cancel for 'No'")
+      console.log (uppercase);
 
-  lowercase = confirm ("Do you want to include lowerrcase letters? Please select OK for 'Yes' or Cancel for 'No'")
-    console.log (lowercase);
+    lowerCase = confirm ("Do you want to include lowerrcase letters? Please select OK for 'Yes' or Cancel for 'No'")
+      console.log (lowercase);
 
-  // NUMBER OPTION
-  numbers = confirm ("Do you want to include numbers? Please select OK for 'Yes' or Cancel for 'No'")
-    console.log (numbers)
+    // NUMBER OPTION
+    numbers = confirm ("Do you want to include numbers? Please select OK for 'Yes' or Cancel for 'No'")
+      console.log (numbers)
 
-  // SPECIAL CHARACTERS OPTION
-  special = confirm ("Do you want to include special characters? Please select OK for 'Yes' or Cancel for 'No'")
-    console.log (special);
+    // SPECIAL CHARACTERS OPTION
+    specialCharacters = confirm ("Do you want to include special characters? Please select OK for 'Yes' or Cancel for 'No'")
+      console.log (special);
   };  
 
-  // Password to the #password input
+  // CONCATINATION 
+  var possiblePasswordArray = []
+  var passwordArray []
+
+  if (upperCase) {
+    possiblePasswordArray = possiblePasswordArray.concat (upperCase);
+  }
+  
+  if (lowerCase) {
+    possiblePasswordArray = possiblePasswordArray.concat (lowerCase);
+  }
+
+  if (numbers) {
+    possiblePasswordArray = possiblePasswordArray.concat (numbers);
+  }
+
+  if (specialCharacters) {
+    possiblePasswordArray = possiblePasswordArray.concat (specialCharacters)
+  }
+
+
+// Password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-
 };
 
 // Get references to the #generate element
